3b9eb4764fdffaaf1a72be9d98e6b02437566cda
Alexander Whillas <whillas@gmail.com>
2017-02-09 11:33:37 +1100

    Static files serving on Production setup i.e. whitenoise

--- config/settings.py
+++ config/settings.py
@@ -15,11 +15,20 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "mindikatta", "static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -39,8 +48,6 @@ LOGOUT_REDIRECT_URL = '/'
 
 FIXTURE_DIRS = ("mindikatta/harvest/fixtures/",)
 
-# The weight to mulitipuly the counter by to get the weight of a counter weighing
-HARVEST_WEIGHT_PER_COUNT = 25
 
 # Application definition
 
@@ -138,14 +145,16 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
-STATIC_URL = '/static/'
-
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Crispy forms
 
 INSTALLED_APPS += ['crispy_forms']
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Harvest
+
+# The weight to mulitipuly the counter by to get the weight of a counter weighing
+HARVEST_WEIGHT_PER_COUNT = 25
