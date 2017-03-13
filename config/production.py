81bdaa789586943f121c5ce15a9c2ccc0f8c31c5
Alexander Whillas <whillas@gmail.com>
2017-03-13 11:11:15 +0100

    Setup Anymail with SendGrid on production

--- config/production.py
+++ config/production.py
@@ -8,3 +8,19 @@ ALLOWED_HOSTS = ['farm.whillas.com', 'mindikatta.herokuapp.com']
 DATABASES = {
     'default': dj_database_url.config()
 }
+
+
+# Email setup
+# see: https://anymail.readthedocs.io/en/stable/installation/
+
+INSTALLED_APPS += ["anymail"]
+
+EMAIL_BACKEND = "anymail.backends.sendgrid.SendGridBackend"
+
+ANYMAIL = {
+    'SENDGRID_API_KEY': os.getenv('SENDGRID_API_KEY'),
+    'SENDGRID_USERNAME': os.getenv('SENDGRID_USERNAME'),
+    'SENDGRID_PASSWORD': os.getenv('SENDGRID_PASSWORD'),
+}
+
+DEFAULT_FROM_EMAIL = 'Mindikatta Webmaster <whillas@gmail.com>'
