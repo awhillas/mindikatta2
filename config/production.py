167c413d2c43a04430ab9e9eaae1ff4ad982bd2f
whillas <whillas@gmail.com>
2024-07-29 05:53:02 +1200

    Revert back to using Supabase (for now)

--- config/production.py
+++ config/production.py
@@ -1,6 +1,6 @@
 import os
 
-# import dj_database_url
+import dj_database_url
 from config.settings import *
 
 print("Using production settings")
@@ -10,26 +10,20 @@ ALLOWED_HOSTS = [
 ]
 
 # override databases, use Heroku's
-# DATABASES = {"default": dj_database_url.config()}
-# DB Secrets from the k8s secret
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("POSTGRES_DB", "mindikatta_django"),
-        "USER": os.getenv("POSTGRES_USER", "postgres"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
-        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
-        "PORT": "5432",
-    }
-}
+
+DATABASES = {"default": dj_database_url.config()}
 
 STATIC_ROOT = os.getenv("STATIC_ROOT", "/data/static")
 
 # Email setup
 # see: https://anymail.readthedocs.io/en/stable/installation/
+
 # INSTALLED_APPS += ["anymail"]
+
 # EMAIL_BACKEND = "anymail.backends.sendgrid.SendGridBackend"
+
 # ANYMAIL = {
 #     "SENDGRID_API_KEY": "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"
 # }
+
 # DEFAULT_FROM_EMAIL = "Mindikatta Webmaster <whillas@gmail.com>"
